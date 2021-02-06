d6ef4a4ecf2576e575eb0103efab4cf2605fd6d3
nitro583 <seiyamatsumoto@me.com>
2021-02-07 00:14:33 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="page">
+    <ul>
+      <li v-for="todo in todos" :key="todo">
+        {{ todo }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: ['これと', 'あれと', 'それ'],
+    }
+  }
+};
+</script>
